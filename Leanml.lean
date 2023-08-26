6bcfcaa63b7e5e82220fc6301c75c15afb38c6d2
Peiyang-Song <p_song@ucsb.edu>
2023-08-26 09:05:22 -0700

    Return mutliple tactics

--- Leanml.lean
+++ Leanml.lean
@@ -13,7 +13,8 @@ open Lean Elab Parser Term Meta Tactic
 elab "trace_goal_state" : tactic => do
   let goals ← getUnsolvedGoals
   let msg ← MessageData.toString <| ← addMessageContext <| goalsToMessageData goals
-  logInfo <| s!"[GOAL STATE] {coreFun msg}"
+  -- return results of five calls to coreFun, each in a new line.
+  logInfo <| s!"[TACTIC CANDIDATES] {coreFun msg} {coreFun msg} {coreFun msg} {coreFun msg} {coreFun msg}"
 
 def main : IO Unit :=
   IO.println s!"Hello!"
