830e4c34e95da989cd8e8583dda8fae121b4512f
Peiyang-Song <p_song@ucsb.edu>
2023-08-26 03:08:53 -0700

    Integrate interface with main function

--- Leanml.lean
+++ Leanml.lean
@@ -13,7 +13,7 @@ open Lean Elab Parser Term Meta Tactic
 elab "trace_goal_state" : tactic => do
   let goals ← getUnsolvedGoals
   let msg ← MessageData.toString <| ← addMessageContext <| goalsToMessageData goals
-  logInfo <| s!"[GOAL STATE] {msg}"
+  logInfo <| s!"[GOAL STATE] {coreFun msg}"
 
 def main : IO Unit :=
   IO.println s!"Hello!"
