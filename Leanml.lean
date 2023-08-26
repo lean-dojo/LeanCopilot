877c20a662e9832c0e7189c1aaa87eba5fdcdcd2
Peiyang-Song <p_song@ucsb.edu>
2023-08-26 09:09:25 -0700

    Arrange tactic candidates

--- Leanml.lean
+++ Leanml.lean
@@ -14,7 +14,12 @@ elab "trace_goal_state" : tactic => do
   let goals ← getUnsolvedGoals
   let msg ← MessageData.toString <| ← addMessageContext <| goalsToMessageData goals
   -- return results of five calls to coreFun, each in a new line.
-  logInfo <| s!"[TACTIC CANDIDATES] {coreFun msg} {coreFun msg} {coreFun msg} {coreFun msg} {coreFun msg}"
+  let tactic_candidate0 := coreFun msg
+  let tactic_candidate1 := coreFun msg
+  let tactic_candidate2 := coreFun msg
+  let tactic_candidate3 := coreFun msg
+  let tactic_candidate4 := coreFun msg
+  logInfo <| s!"[TACTIC CANDIDATES]\n{tactic_candidate0}\n{tactic_candidate1}\n{tactic_candidate2}\n{tactic_candidate3}\n{tactic_candidate4}"
 
 def main : IO Unit :=
   IO.println s!"Hello!"
