f2c9427b691c99371cd4b767ecddc601dc16936b
Kaiyu Yang <kaiyuy@cs.princeton.edu>
2023-09-11 20:12:23 -0500

    remove deps

--- lakefile.lean
+++ lakefile.lean
@@ -20,18 +20,73 @@ lean_lib Examples {
 }
 
 
-def buildCpp (path pkgDir buildDir : FilePath) (buildType : BuildType) : SchedulerM (BuildJob FilePath) := do
-  let optLevel := if buildType == BuildType.release then "-O3" else "-O0"
+def onnxVersion := "1.15.1"
+def onnxPlatform := if System.Platform.isOSX then "osx-universal2" else "linux-x64"
+def onnxFileStem := s!"onnxruntime-{onnxPlatform}-{onnxVersion}"
+def onnxFilename := s!"{onnxFileStem}.tgz"
+def onnxUrl := "https://github.com/microsoft/onnxruntime/releases/download/v1.15.1/" ++ onnxFilename
+
+
+target downloadOnnxRuntime : FilePath := Job.async do 
+  logInfo s!"Downloading ONNX Runtime library"
+  if System.Platform.isWindows then
+    panic! "Windows is not supported"
+  if System.Platform.numBits != 64 then
+    panic! "Only 64-bit platforms are supported"
+
+  -- TODO: Download to a temporary directory.
+  try
+    let depTrace := Hash.ofString onnxUrl
+    let trace ← buildFileUnlessUpToDate onnxFilename depTrace do
+      download onnxFilename onnxUrl onnxFilename
+      untar onnxFilename onnxFilename (← IO.currentDir)
+    return (onnxFileStem, trace)
+  else
+    return (onnxFileStem, .nil)
+  /-
+  let some stem := (filename: FilePath).fileStem | panic! "unexpected filename: {filename}"
+  let libDir : FilePath := stem / "lib"
+  let includeDir : FilePath := stem / "include"
+  return (includeDir, libDir)
+  -/
+
+
+target libonnxruntime pkg : FilePath := do
+  logStep s!"Packaging the ONNX Runtime library"
+  let onnx ← fetch $ pkg.target ``downloadOnnxRuntime
+  let srcFile : FilePath := onnxFileStem / "lib" / (nameToSharedLib s!"onnxruntime.{onnxVersion}")
+  let src ← inputFile $ srcFile
+  let dst := pkg.nativeLibDir / (nameToSharedLib "onnxruntime")
+  let dst' := pkg.nativeLibDir / (nameToSharedLib s!"onnxruntime.{onnxVersion}")
+  createParentDirs dst
+  buildFileAfterDepList dst [onnx, src] fun deps => do
+    proc {
+      cmd := "cp"
+      args := #[deps[1]!.toString, dst.toString]
+    }
+    proc {
+      cmd := "ln"
+      args := #["-s", dst.toString, dst'.toString]
+    }
+    proc {
+      cmd := "cp"
+      args := #["-r", (onnxFileStem ++ "/include"), (pkg.buildDir / "include").toString]
+    }
+
+
+def buildCpp (pkg : Package) (path : FilePath) (onnx : BuildJob FilePath) : SchedulerM (BuildJob FilePath) := do
+  let optLevel := if pkg.buildType == BuildType.release then "-O3" else "-O0"
   let flags := #["-fPIC", "-std=c++11", "-stdlib=libc++", optLevel]
-  let args := flags ++ #["-I", (← getLeanIncludeDir).toString]
-  let oFile := buildDir / (path.withExtension ".o")
-  let srcJob ← inputFile <| pkgDir / path
-  buildFileAfterDep oFile srcJob (extraDepTrace := computeHash flags) fun srcFile =>
-    compileO path.toString oFile srcFile args "clang++"
+  let args := flags ++ #["-I", (← getLeanIncludeDir).toString, "-I", (pkg.buildDir / "include").toString]
+  let oFile := pkg.buildDir / (path.withExtension "o")
+  let srcJob ← inputFile <| pkg.dir / path
+  buildFileAfterDepList oFile [srcJob, onnx] (extraDepTrace := computeHash flags) fun deps =>
+    compileO path.toString oFile deps[0]! args "clang++"
 
 
 target generator.o pkg : FilePath := do
-  let build := buildCpp "cpp/generator.cpp" pkg.dir pkg.buildDir pkg.buildType
+  let onnx ← fetch $ pkg.target ``libonnxruntime
+  let build := buildCpp pkg "cpp/generator.cpp" onnx
   if pkg.name ≠ (← getRootPackage).name then
     (← pkg.fetchFacetJob `release).bindAsync fun _ _ => build
   else
@@ -39,52 +94,16 @@ target generator.o pkg : FilePath := do
     
   
 target retriever.o pkg : FilePath := do
-  let build := buildCpp "cpp/retriever.cpp" pkg.dir pkg.buildDir pkg.buildType
+  let onnx ← fetch $ pkg.target ``libonnxruntime
+  let build := buildCpp pkg "cpp/retriever.cpp" onnx
   if pkg.name ≠ (← getRootPackage).name then
     (← pkg.fetchFacetJob `release).bindAsync fun _ _ => build
   else
     build
 
 
-def onnxVersion := "1.15.1"
-
-
-def downloadOnnxRuntime : LogIO FilePath := do 
-  logInfo s!"Downloading ONNX Runtime library"
-  if System.Platform.isWindows then
-    panic! "Windows is not supported"
-  if System.Platform.numBits != 64 then
-    panic! "Only 64-bit platforms are supported"
-  let platform := if System.Platform.isOSX then "osx-universal2" else "linux-x64"
-  let filename := s!"onnxruntime-{platform}-{onnxVersion}.tgz"
-  let url := "https://github.com/microsoft/onnxruntime/releases/download/v1.15.1/" ++ filename
-  -- TODO: Download to a temporary directory.
-  download filename url filename
-  untar filename filename (← IO.currentDir)
-  let some stem := (filename: FilePath).fileStem | panic! "unexpected filename: {filename}"
-  return stem / "lib" / (nameToSharedLib s!"onnxruntime.{onnxVersion}")
-
-
-target libonnxruntime pkg : FilePath := do
-  logStep s!"Packaging the ONNX Runtime library"
-  let src ← inputFile (← downloadOnnxRuntime)
-  let dst := pkg.nativeLibDir / (nameToSharedLib "onnxruntime")
-  let dst' := pkg.nativeLibDir / (nameToSharedLib s!"onnxruntime.{onnxVersion}")
-  createParentDirs dst
-  buildFileAfterDep dst src fun srcFile => do
-    proc {
-      cmd := "cp"
-      args := #[srcFile.toString, dst.toString]
-    }
-    proc {
-      cmd := "ln"
-      args := #["-s", dst.toString, dst'.toString]
-    }
-
-
 extern_lib libleanffi pkg := do
   let name := nameToStaticLib "leanffi"
-  let _ ← fetch <| pkg.target ``libonnxruntime
   let oGen ← fetch <| pkg.target ``generator.o
   let oRet ← fetch <| pkg.target ``retriever.o
   buildStaticLib (pkg.nativeLibDir / name) #[oGen, oRet]
