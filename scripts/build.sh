131b0ffe54224478c3104cda7f592d72cbb1010e
Kaiyu Yang <kaiyuy@caltech.edu>
2023-11-05 12:34:24 -0800

    simplify build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,18 +4,10 @@
 # 1. Launch a codespace for LeanInfer.
 # 2. Run `source scripts/build.sh`.
 
-# Set up LLVM.
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.0/clang+llvm-16.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
-tar -xf clang+llvm-16.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz -C /workspaces/ && rm clang+llvm-16.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
-LLVM_HOME="/workspaces/clang+llvm-16.0.0-x86_64-linux-gnu-ubuntu-18.04"
-export PATH="${LLVM_HOME}/bin":$PATH
-export LIBRARY_PATH="${LLVM_HOME}/lib/x86_64-unknown-linux-gnu":$LIBRARY_PATH
-export LD_LIBRARY_PATH="${LLVM_HOME}/lib/x86_64-unknown-linux-gnu":$LD_LIBRARY_PATH
-
 # Set up elan.
 curl https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh -sSf | bash -s -- -y
 source $HOME/.elan/env
 
 # Build the project.
 lake build
-lake build Examples
+lake build LeanInferTests
