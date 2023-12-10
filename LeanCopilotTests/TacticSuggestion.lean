a3b30b153357faa551f7eae68382566118eb1631
Kaiyu Yang <kaiyuy@caltech.edu>
2023-12-10 15:18:13 -0800

    minor fix

--- LeanCopilotTests/TacticSuggestion.lean
+++ LeanCopilotTests/TacticSuggestion.lean
@@ -63,7 +63,11 @@ example (a b c : Nat) : a + b + c = a + c + b := by
 
 /-
 ### Bring Your Own Model
+
+1. Make sure the model is up and running, e.g., by going to ./python and running `uvicorn server:app --port 23337`.
+2. Uncomment the code below.
 -/
+
 
 def myModel : ExternalGenerator := {
   name := "wellecks/llmstep-mathlib4-pythia2.8b"
@@ -74,9 +78,11 @@ def myModel : ExternalGenerator := {
 
 #eval registerGenerator "wellecks/llmstep-mathlib4-pythia2.8b" (.external myModel)
 
-
+/-
 set_option LeanCopilot.suggest_tactics.check false in
 set_option LeanCopilot.suggest_tactics.model "wellecks/llmstep-mathlib4-pythia2.8b" in
 example (a b c : Nat) : a + b + c = a + c + b := by
   suggest_tactics
   sorry
+
+-/
