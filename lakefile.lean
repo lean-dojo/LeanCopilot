5121a1fed77dce78c76073d710d33ba2f16343f7
Peiyang-Song <p_song@ucsb.edu>
2023-12-10 16:22:55 -0800

    Change to release

--- lakefile.lean
+++ lakefile.lean
@@ -85,7 +85,7 @@ package LeanCopilot where
   preferReleaseBuild := get_config? noCloudRelease |>.isNone
   buildArchive? := buildArchiveName
   precompileModules := true
-  buildType := BuildType.debug  -- TODO: Release
+  buildType := BuildType.release
   moreLinkArgs := #[s!"-L{__dir__}/.lake/build/lib", "-lctranslate2"]
   weakLeanArgs := #[s!"--load-dynlib={__dir__}/.lake/build/lib/" ++ nameToSharedLib "ctranslate2"]
 
