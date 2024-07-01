b5cc5b6a25b8a241fbe7cc99fe548070a13720b2
Peiyang Song <p_song@ucsb.edu>
2024-07-01 13:14:17 +0800

    Bump to Lean v4.10.0-rc1 with dependencies

--- lakefile.lean
+++ lakefile.lean
@@ -302,8 +302,8 @@ extern_lib libleanffi pkg := do
   buildStaticLib (pkg.nativeLibDir / name) #[ct2O]
 
 
-require batteries from git "https://github.com/leanprover-community/batteries.git" @ "54bb04c3119f24fde14b9068c4b2e69db52a1450" -- Lean v4.9.0
-require aesop from git "https://github.com/leanprover-community/aesop" @ "06cca4bd36b2af743d4858c5cc31604aa9da26bc" -- Lean v4.9.0
+require batteries from git "https://github.com/leanprover-community/batteries.git" @ "2ead90d24b4fac3a05c9c4294daa39bd8686fb98" -- Lean v4.10.0-rc1
+require aesop from git "https://github.com/leanprover-community/aesop" @ "a64fe24aa94e21404940e9217363a9a1ed9a33a6" -- Lean v4.10.0-rc1
 
 meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
-require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "1daa0de7eea6bf8d3e1b0814156ede21a8a9e9e9" -- Lean v4.9.0
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "c369436d21c583a60da62d4513c7b9ea08389074" -- Lean v4.10.0-rc1
