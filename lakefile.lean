0739a2af31ffa3e92cdded8c1f10fa1f70c6fe96
psong <psong@caltech.edu>
2025-06-16 21:10:56 -0700

    Bump to latest unstable version

--- lakefile.lean
+++ lakefile.lean
@@ -431,8 +431,8 @@ extern_lib libleanffi pkg := do
   buildStaticLib (pkg.sharedLibDir / name) #[ct2O]
 
 
-require batteries from git "https://github.com/leanprover-community/batteries.git" @ "8d2067bf518731a70a255d4a61b5c103922c772e"
-require aesop from git "https://github.com/leanprover-community/aesop" @ "8ff27701d003456fd59f13a9212431239d902aef"
+require batteries from git "https://github.com/leanprover-community/batteries.git" @ "main"
+require aesop from git "https://github.com/leanprover-community/aesop" @ "master"
 
 meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
